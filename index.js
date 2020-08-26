25a76cc98d4689611b1b85795b7c27c55296cf3b
TJ Taylor <tjtaylorjr@gmail.com>
2020-08-26 16:15:46 -0400

    completed phase 2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const { text } = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(express.urlencoded());
+
+const csrfProtection = csrf({cookie: true});
+
+
 
 app.get("/", (req, res) => {
-  res.render('index', { users })
+  res.render('index', { users, fields })
+});
+
+app.get("/create", (req, res) => {
+  res.render('form', { users, fields })
 });
 
 const users = [
@@ -17,6 +30,30 @@ const users = [
     email: "jill.jack@gmail.com"
   }
 ];
+
+const fields = [
+  {
+    type: "text",
+    name: "firstName"
+  },
+  {
+    type: "text",
+    name: "lastName"
+  },
+  {
+    type: "email",
+    name: "email"
+  },
+  {
+    type: "password",
+    name: "password"
+  },
+  {
+    type: "password",
+    name: "confirmedPassword"
+  }
+]
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
