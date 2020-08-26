b5cd0ac8a1b41ae41b7ddf296eca9f9d2f636d09
Ivan Roth <ivan.h.roth@gmail.com>
2020-08-26 14:19:14 -0700

    Phase 3

--- index.js
+++ index.js
@@ -15,11 +15,42 @@ const csrfProtection = csrf({cookie: true});
 
 
 app.get("/", (req, res) => {
-  res.render('index', { users, fields })
+  res.render('index', { users, fields });
 });
 
-app.get("/create", (req, res) => {
-  res.render('form', { users, fields })
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('form', { users, fields, errors: [], csrfToken: req.csrfToken() });
+});
+
+app.post("/create", csrfProtection, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = [];
+
+  if(!firstName) errors.push("Please provide a first name.");
+  if(!lastName) errors.push("Please provide a last name.");
+  if(!email) errors.push("Please provide an email.");
+  if(!password) errors.push("Please provide a password.");
+  if(!confirmedPassword) errors.push("Please confirm your password.");
+  if(password !== confirmedPassword) errors.push("The provided values for the password and password confirmation fields did not match.");
+
+  if(errors.length === 0) {
+    let newUser = { firstName, lastName, email, password, id: users[users.length-1].id + 1 };
+    users.push(newUser);
+    res.redirect("/");
+    return;
+  }
+
+  fields[0].value = firstName;
+  fields[1].value = lastName;
+  fields[2].value = email;
+
+  res.render('form', { users, fields, errors, csrfToken: req.csrfToken() });
+
+  fields[0].value = null;
+  fields[1].value = null;
+  fields[2].value = null;
+
+  return;
 });
 
 const users = [
